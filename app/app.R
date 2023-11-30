02d18be31df639ee58a7d0479d785f89712f9116
Alek <alek.rutkowski@gmail.com>
2023-11-30 17:34:03 +0100

    first proto ui, JSON data not needed (Rds instead)

--- app/app.R
+++ app/app.R
@@ -1,6 +1,5 @@
 library(shiny)
-library(shinyjs)
-
+library(plotly)
 library(data.table)
 library(magrittr)
 
@@ -9,27 +8,107 @@ IS_SHINYLIVE <-
 
 DATA <- 
   'data.Rds' %>% 
-  {if (IS_SHINYLIVE)
-    (.) %T>% 
-      download.file(paste0('https://raw.githubusercontent.com/alekrutkowski/JAF2R_shinylive/main/data/',
-                           .),.) else
+  {`if`(IS_SHINYLIVE,
+        (.) %T>% 
+          download.file(
+            paste0('https://raw.githubusercontent.com/alekrutkowski/JAF2R_shinylive/main/data/',
+                   .),
+            .),
         paste0('../data/',.)
-  } %>% 
+  )} %>% 
   readRDS()
 
+INDICATORS <-
+  DATA$JAF_SCORES %>% 
+  .[,.(JAF_KEY,Description)] %>% 
+  unique() %>% 
+  .[, JAF_KEY__Description :=
+      paste0('[',JAF_KEY,'] ',Description)] %>% 
+  {set_names(.$JAF_KEY,.$JAF_KEY__Description)} # key/name = '[JAF_KEY] Description', value = 'JAF_KEY'
+
+GEOS <-
+  DATA$EU_Members_geo_names %>% 
+  .[, geo__geo_labels :=
+      paste0('[',geo,'] ',geo_labels)] %>% 
+  {set_names(.$geo,.$geo__geo_labels)} # key/name = '[geo] geo_labels', value = 'geo'
+
+YEARS <-
+  DATA$JAF_GRAND_TABLE_reduced %>% 
+  unique() %>% 
+  .$time
+
 ui <- fluidPage(
-  useShinyjs(),  # Initialize shinyjs
-  titlePanel("JAF indicators"),
-  actionButton("doCalc", "Do some calculation"),
-  tableOutput("dataOutput")
+  tags$head(
+    tags$style(HTML("
+    .flex-container {
+      display: flex;
+      justify-content: space-between;
+    }
+    .left-text {
+      /* Optional styling for left text */
+    }
+    .right-text {
+      /* Optional styling for right text */
+    }
+    "))),
+  HTML('
+    <div class="flex-container">
+      <div class="left-text"><h1>JAF Indicators</h1></div>
+      <div class="right-text">
+      <img src="https://raw.githubusercontent.com/alekrutkowski/JAF2R/main/JAF2R_logo_v3.png" alt="JAF2R project logo" height="40"/>
+      </div>
+    </div>'),
+  selectInput(
+    inputId = "SelectedIndics",
+    label = HTML("<strong>Select one or more indicators &ndash; start typing codes or names, use <kbd>Delete</kbd> or <kbd>Backspace</kbd> keyboard keys to delete</strong>"),
+    choices = INDICATORS,
+    multiple=TRUE,
+    width = "100%"
+  ),
+  checkboxGroupInput(
+    inputId = "SelectedScore",
+    label = strong("Show standardised scores (latest year) or values (one or more years)"),
+    choices = c("Tick for scores, untick for values" = TRUE),
+    width = "100%"
+  ),
+  selectInput(
+    inputId = "SelectedGeos",
+    label = HTML("<strong>Select countries or country aggregates, use <kbd>Delete</kbd> or <kbd>Backspace</kbd> keyboard keys to delete</strong>"),
+    choices = GEOS,
+    multiple=TRUE,
+    width = "100%"
+  ),
+  sliderInput(
+    inputId = "SelectedYears",
+    label = strong("Select a start year for time series (if you want to see a shorter period)"),
+    min = min(YEARS),
+    max = max(YEARS)-1,
+    ticks=FALSE,
+    sep="",
+    value = min(YEARS),
+    width = "100%",
+    step = 1
+  ),
+  tabsetPanel(
+    tabPanel("Chart",
+             plotlyOutput("ThePlot")),
+    tabPanel("Table",
+             tableOutput("TheTable"))
+  )
+  
 )
 
 server <- function(input, output) {
   
-  output$dataOutput <- renderTable({
+  output$TheTable <- renderTable({
     DATA %>% 
       .$JAF_SCORES %>% 
       head(20)
+  })
+  
+  output$ThePlot <- renderPlotly({
+    # generate bins based on input$bins from ui.R
+    plot_ly(x = ~ faithful[, 2], type = "histogram")
   })
   
 }
