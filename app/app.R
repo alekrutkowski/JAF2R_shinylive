fb3fa0eb6b8b020f46ef0779e3d3944d503538c6
Alek <alek.rutkowski@gmail.com>
2023-12-04 18:19:59 +0100

    implemented histogram (TODO: p25, p75, min max)

--- app/app.R
+++ app/app.R
@@ -2,9 +2,12 @@ library(shiny)
 library(plotly)
 library(data.table)
 library(magrittr)
+library(kit)
 
 IS_SHINYLIVE <- 
   grepl("wasm",R.Version()$arch)
+
+`%not in%` <- Negate(`%in%`)
 
 DATA <- 
   'data.Rds' %>% 
@@ -46,41 +49,94 @@ GEOS <-
   DATA$EU_Members_geo_names %>% 
   .[, geo__geo_labels :=
       paste0('[',geo,'] ',geo_labels)] %>% 
-  {set_names(.$geo,.$geo__geo_labels)} # key/name = '[geo] geo_labels', value = 'geo'
+  {set_names(as.character(.$geo),.$geo__geo_labels)} # key/name = '[geo] geo_labels', value = 'geo'
 
 YEARS <-
   DATA$JAF_GRAND_TABLE_reduced %>% 
   unique() %>% 
   .$time
 
-`%not in%` <- Negate(`%in%`)
+ifScoresSelected <- function(input, x, y) {
+  # str(input)
+  if (identical(input$SelectedScore,'TRUE')) x else y
+}
 
-ifScoresSelected <- function(input, x, y)
-  if (input$SelectedScore) x else y
+ifLevelsSelected <- function(input, x, y)
+  if (identical(input$SelectedLevel,'TRUE')) x else y
+
+selectedVarname <- function(input) {
+  #   message('~~~~~~~~~~~~~~'); str(input$SelectedScore); str(input$SelectedLevel); message('~~~~~~~~~~~~~~')
+  x <- identical(input$SelectedScore,'TRUE')
+  y <- identical(input$SelectedLevel,'TRUE')
+  kit::nif(x && y, 'score_latest_value',
+           x && !y, 'score_change',
+           !x && y, 'value_',
+           !x && !y, 'value_change')  }
 
 filteredDATA <- function(input)
   DATA %>% 
-  .[[ifScoresSelected('JAF_SCORES','JAF_GRAND_TABLE_reduced')]] %>% 
+  .[[ifScoresSelected(input, 'JAF_SCORES','JAF_GRAND_TABLE_reduced')]] %>% 
   .[JAF_KEY %in% filteredSelectedIndics(input$SelectedIndics)]
-  # TODO pick the right var: score or value, level or change
 
 hist. <- function(input) {
   dta <-
-    input %>% 
-    filteredDATA() %>% 
+    filteredDATA(input) %>% 
     .[, .SD[time==max(time)], by=geo]
-  plot_ly(x=dta[geo %in% EU_Members_geo_names],
-          type="histogram",
+  var.. <-
+    selectedVarname(input)
+  val.. <-
+    dta[geo==input$SelectedGeos, var.., with=FALSE] %>% as.numeric()
+  hist.. <-
+    dta %>% 
+    .[geo %not in% c(DATA$EU_geo_code,DATA$EA_geo_code), 
+      c(var..,'geo'), with=FALSE] %>% {
+        d. <- (.)
+        h. <- hist(d.[[var..]],plot=FALSE)
+        l. <- d. %>% 
+          .[, in_bin_no := 
+              cut(d.[[var..]], breaks=h.$breaks,
+                  include.lowest=TRUE, right=FALSE, labels=F)] %>% 
+          .[, .(l = geo %>% 
+                  paste(collapse=', ') %>% 
+                  strwrap(10) %>% 
+                  paste(collapse='\n'))
+            , by=in_bin_no] %>% 
+          setorder(in_bin_no)
+        data.table(in_bin_no=seq_along(h.$mids),
+                   x=h.$mids,
+                   y=h.$counts) %>% 
+          merge(l., by='in_bin_no', all.x=TRUE)
+      }
+  plot_ly(x=hist..$x, y=hist..$y,
+          text=hist..$l,
+          type="bar",
           marker = list(color='#D3D3D3'),
           name="All countries") %>% 
-    layout(title="Overall Plot Title",
-           xaxis=list(title=ifScoresSelected('Score', 'Indicator value')),
-           yaxis=list(title='Number of countries inside each interval'))  %>% 
-    add_trace(name=GEOS[input$SelectedGeos],
-              x = c(1, 1), # TODO replace 1s with the value for the selected geo,
-              y = c(0, 20), # TODO replace 20 with the max bin height 
-              type = "scatter", mode = "lines", 
-              line = list(color = 'red', width = 2))
+    layout(title=paste(names(INDICATORS)[INDICATORS==input$SelectedIndics],
+                       '\nThe value for',names(GEOS)[GEOS==input$SelectedGeos],
+                       'and the distribution of values across other countries'),
+           xaxis=list(title=paste0(ifScoresSelected(input, 'Score ', 'Indicator value '),
+                                   ifLevelsSelected(input, '(level)', '(change)'))),
+           yaxis=list(title='Number of countries inside each interval'),
+           bargap = 0,
+           margin = list(t = 60), # more space at the top for the title
+           annotations = list(
+             x = val..,
+             y = max(hist..$y),
+             text = round(val..,1),
+             showarrow = TRUE,
+             arrowhead = 5,
+             font = list(size = 18)
+           )) %>%
+    add_trace(name=names(GEOS)[GEOS==input$SelectedGeos],
+              x = val.. %>%
+                c(., .),
+              y = c(0, max(hist..$y)),
+              text = c("",""), # mandatory
+              type = "scatter", mode = "lines",
+              marker = list(opacity=0),
+              line = list(color = 'black', width = 3))
+  # add p25, p75, min, max (last two greyish)
 }
 
 
@@ -126,13 +182,13 @@ ui <- fluidPage(
     column(4,checkboxGroupInput(
       inputId = "SelectedScore",
       label = strong("Show standardised scores (latest year) or values (one or more years)?"),
-      choices = c("Tick for scores, untick for values" = TRUE),
+      choices = c("Tick for scores, untick for values" = 'TRUE'),
       width = "100%"
     )),
     column(4,checkboxGroupInput(
       inputId = "SelectedLevel",
       label = strong("Show levels or changes?"),
-      choices = c("Tick for levels, untick for changes" = TRUE),
+      choices = c("Tick for levels, untick for changes" = 'TRUE'),
       width = "100%"
     )),
     column(4,sliderInput(
@@ -164,7 +220,8 @@ server <- function(input, output) {
   })
   
   output$ThePlot <- renderPlotly({
-    plot_ly(x = ~ faithful[, 2], type = "histogram")
+    # TODO add plot type dispatch function based on the length of selected inputs (countries, indics, years)
+    hist.(input)
   })
   
 }
